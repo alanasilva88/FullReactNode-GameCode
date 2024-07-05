2693ef8e9ef80623493210cc558ecabfdda18d2d
Nathália Ribeiro <nat_riba@hotmail.com>
2024-07-05 14:09:57 -0300

    atualizacao galeria jogos

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -2,11 +2,13 @@ import "./Galeria.css";
 import Card from './Card';
 
 const jogos = [
-  { cod: 1, titulo: 'The Last of Us Part II', descricao: 'Ação/Aventura', preco: 'R$199,99' },
+  { cod: 1, titulo: 'The Last of Us', descricao: 'Ação/Aventura', preco: 'R$199,99' },
   { cod: 2, titulo: 'God of War', descricao: 'Ação/Aventura', preco: 'R$149,99' },
   { cod: 3, titulo: 'Spider-Man', descricao: 'Ação/Aventura', preco: 'R$99,99' },
   { cod: 4, titulo: 'Resident Evil', descricao: 'Ficção/Ação', preco: 'R$299,90'},
-  // Adicione mais jogos aqui
+  { cod: 5, titulo: 'Mario Bros', descricao: 'Ação/Aventura', preco: 'R$199,99' },
+  { cod: 6, titulo: 'Mario Kart', descricao: 'Aventura', preco: 'R$159,90'},
+  
 ];
 
 const Galeria = () => {
